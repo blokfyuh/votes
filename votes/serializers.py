2359f7b1af21a459ebe48e5870fbf6840584ba54
Alexandre Boukhlif <blokfyuh@users.noreply.github.com>
2017-07-17 16:14:29 -0300

    Added fields = __all__ because there is no implic
    it fields creation from now on.

--- votes/serializers.py
+++ votes/serializers.py
@@ -1,9 +1,6 @@
 
 """ Serializers for api support on Votes app. """
 
-# Core Django imports
-from django.db import transaction
-
 # Third party imports
 from rest_framework import serializers
 
@@ -15,6 +12,7 @@ class VoteSerializer(serializers.ModelSerializer):
 
     """ Returns serialized data of Vote instances. """
 
-    @transaction.atomic
     class Meta:
         model = Vote
+        fields = '__all__'
+        
